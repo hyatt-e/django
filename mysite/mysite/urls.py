eb3a826a9f19cf9d5882c03aa76fb4c7bde2f2ff
hyatt-e <hyatt1ea@gmail.com>
2019-04-08 16:42:12 -0400

    Created Tutorial model and made/migrated migrations

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+	# this passes the home url to the main.urls page
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
