be0d2013cdd6057cfad9e0fde11abb075692fd0e
hyatt-e <hyatt1ea@gmail.com>
2019-04-11 15:23:44 -0400

    Added logout and login functionality, including login page.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -37,3 +37,33 @@ def register(request):
     return render(request,
                   "main/register.html",
                   context={"form":form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully")
+    return redirect("main:homepage")
+
+
+def login_request(request):
+    # request by default is only GET. Must specify a POST request
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, f"Invalid username or password")
+                return redirect("main:homepage")
+        else:
+            messages.error(request, f"Invalid username or password")
+            return redirect("main:homepage")
+
+    form = AuthenticationForm()
+    return render(request,
+                  "main/login.html", 
+                  {"form":form})
